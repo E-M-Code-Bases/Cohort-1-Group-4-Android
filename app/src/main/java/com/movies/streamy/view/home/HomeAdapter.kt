fdfc5c1ddd6011153fb44b270941da2b2d5902ae
swafak <swafakorane@gmail.com>
2024-06-24 12:13:49 +0300

    Home adapter updates

--- app/src/main/java/com/movies/streamy/view/home/HomeAdapter.kt
+++ app/src/main/java/com/movies/streamy/view/home/HomeAdapter.kt
@@ -1,5 +1,71 @@
-package com.movies.streamy.view.home
-
+//package com.movies.streamy.view.home
+//
+//import android.content.Context
+//import android.view.LayoutInflater
+//import android.view.ViewGroup
+//import androidx.recyclerview.widget.AsyncListDiffer
+//import androidx.recyclerview.widget.DiffUtil
+//import androidx.recyclerview.widget.RecyclerView
+//import com.bumptech.glide.Glide
+//import com.movies.streamy.R
+//import com.movies.streamy.databinding.AllMovieSlideItemBinding
+//import com.movies.streamy.model.dataSource.network.data.response.homeData.HomeResult
+//
+//class HomeAdapter : RecyclerView.Adapter<HomeAdapter.TransactionsViewHolder> (){
+//    private lateinit var context: Context
+//
+//    override fun onBindViewHolder(holder: TransactionsViewHolder, position: Int) {
+//        val data = asyncList.currentList[position]
+//        holder.bind(data)
+//    }
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionsViewHolder {
+//        context = parent.context
+//        return TransactionsViewHolder(
+//            AllMovieSlideItemBinding.inflate(
+//                LayoutInflater.from(parent.context), parent, false
+//            )
+//        )
+//    }
+//
+//    override fun getItemCount() = asyncList.currentList.size
+//
+//
+//    inner class TransactionsViewHolder(
+//        private val binding: AllMovieSlideItemBinding,
+//    ) : RecyclerView.ViewHolder(binding.root) {
+//        fun bind(data: HomeResult) {
+//            binding.MovieName.text = data.title
+//            binding.Type.text = data.media_type
+//            //binding.genre.text =
+//
+//            data.poster_path?.let { posterPath ->
+//                if (posterPath.isNotEmpty()) {
+//                    Glide.with(binding.image.context)
+//                        .load(posterPath)
+////                        .placeholder(R.drawable.logo) // Optional placeholder image
+//                        .into(binding.image)
+//                } else {
+//                    binding.image.setImageResource(R.drawable.logo) // Placeholder for empty path
+//                }
+//            }
+//            binding.root.setOnClickListener {
+//                //data?.let { clicked.invoke(it) }
+//            }
+//
+//        }
+//    }
+//    private val TransactionsDiffCallback = object : DiffUtil.ItemCallback<HomeResult>() {
+//        override fun areItemsTheSame(oldItem: HomeResult, newItem: HomeResult): Boolean {
+//            return oldItem.id == newItem.id
+//        }
+//
+//        override fun areContentsTheSame(oldItem: HomeResult, newItem: HomeResult): Boolean {
+//            return oldItem == newItem
+//        }
+//    }
+//    val asyncList = AsyncListDiffer(this, TransactionsDiffCallback)
+//}
+//
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -11,7 +77,7 @@ import com.movies.streamy.R
 import com.movies.streamy.databinding.AllMovieSlideItemBinding
 import com.movies.streamy.model.dataSource.network.data.response.homeData.HomeResult
 
-class HomeAdapter : RecyclerView.Adapter<HomeAdapter.TransactionsViewHolder> (){
+class HomeAdapter(private val itemClickListener: OnItemClickListener) : RecyclerView.Adapter<HomeAdapter.TransactionsViewHolder>() {
     private lateinit var context: Context
 
     override fun onBindViewHolder(holder: TransactionsViewHolder, position: Int) {
@@ -33,27 +99,36 @@ class HomeAdapter : RecyclerView.Adapter<HomeAdapter.TransactionsViewHolder> (){
     inner class TransactionsViewHolder(
         private val binding: AllMovieSlideItemBinding,
     ) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setOnClickListener {
+                val position = bindingAdapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val item = asyncList.currentList[position]
+                    itemClickListener.onItemClick(item)
+                }
+            }
+        }
+
         fun bind(data: HomeResult) {
             binding.MovieName.text = data.title
             binding.Type.text = data.media_type
-            //binding.genre.text =
 
             data.poster_path?.let { posterPath ->
                 if (posterPath.isNotEmpty()) {
                     Glide.with(binding.image.context)
                         .load(posterPath)
-//                        .placeholder(R.drawable.logo) // Optional placeholder image
                         .into(binding.image)
                 } else {
-                    binding.image.setImageResource(R.drawable.logo) // Placeholder for empty path
+                    binding.image.setImageResource(R.drawable.logo)
                 }
             }
-            binding.root.setOnClickListener {
-                //data?.let { clicked.invoke(it) }
-            }
-
         }
     }
+
+    interface OnItemClickListener {
+        fun onItemClick(item: HomeResult)
+    }
+
     private val TransactionsDiffCallback = object : DiffUtil.ItemCallback<HomeResult>() {
         override fun areItemsTheSame(oldItem: HomeResult, newItem: HomeResult): Boolean {
             return oldItem.id == newItem.id
@@ -63,6 +138,6 @@ class HomeAdapter : RecyclerView.Adapter<HomeAdapter.TransactionsViewHolder> (){
             return oldItem == newItem
         }
     }
+
     val asyncList = AsyncListDiffer(this, TransactionsDiffCallback)
 }
-
--- app/src/main/java/com/movies/streamy/view/home/HomeFragment.kt
+++ app/src/main/java/com/movies/streamy/view/home/HomeFragment.kt
@@ -1,8 +1,8 @@
 package com.movies.streamy.view.home
 
+import HomeAdapter
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,15 +20,13 @@ import com.movies.streamy.utils.Prefs
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
-
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), HomeAdapter.OnItemClickListener {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var viewModel: HomeViewModel
     private lateinit var prefs: Prefs
-
-    private val homeAdapter: HomeAdapter =  HomeAdapter()
+    private lateinit var homeAdapter: HomeAdapter
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,14 +43,12 @@ class HomeFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+        homeAdapter = HomeAdapter(this)
 
-
-        viewModel.movieList.observe(viewLifecycleOwner, Observer {movieList ->
-            Log.d("HomeFragment", "Fetched Movie List: $movieList")
+        viewModel.movieList.observe(viewLifecycleOwner, Observer { movieList ->
             if (movieList.isNullOrEmpty()) {
                 // Handle empty case if needed
             } else {
-//            hideShimmerEffect()
                 homeAdapter.asyncList.submitList(movieList)
                 binding.AllShows.apply {
                     layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
@@ -61,7 +57,6 @@ class HomeFragment : Fragment() {
                 }
             }
         })
-
 
         return binding.root
     }
@@ -75,26 +70,17 @@ class HomeFragment : Fragment() {
         showShimmerEffect()
         viewModel.getMovieLists()
         setUpObservers()
-        setUpAdapter()
     }
 
     private fun setUpObservers() {
         viewModel.movieList.observe(viewLifecycleOwner, ::setUpRecyclerView)
         viewModel.viewState.observe(viewLifecycleOwner, ::onViewStateChanged)
     }
-    private fun setUpAdapter() {
-        binding.AllShows.apply {
-            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-            setHasFixedSize(true)
-            adapter = homeAdapter
-        }
-    }
+
     private fun setUpRecyclerView(movieList: List<HomeResult?>?) {
-        Log.d("HomeFragment", "Fetched Movie List: $movieList")
         if (movieList.isNullOrEmpty()) {
             // Handle empty case if needed
         } else {
-//            hideShimmerEffect()
             homeAdapter.asyncList.submitList(movieList)
             binding.AllShows.apply {
                 layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
@@ -108,8 +94,11 @@ class HomeFragment : Fragment() {
                 })
             }
             binding.AllShows.post{applyScaleTransformation(binding.AllShows)}
+
+            }
         }
-    }
+
+
     private fun onViewStateChanged(state: HomeViewState) {
         hideShimmerEffect()
         when (state) {
@@ -138,11 +127,11 @@ class HomeFragment : Fragment() {
         binding.AllShows.visibility = View.VISIBLE
     }
 
-    private fun itemClicked(data: HomeResult) {
+    override fun onItemClick(item: HomeResult) {
         // Handle item click
+
     }
 }
-
 private fun applyScaleTransformation(recyclerView: RecyclerView){
     val layoutManager = recyclerView.layoutManager as LinearLayoutManager
     val midpoint = recyclerView.width/2f
@@ -160,3 +149,166 @@ private fun applyScaleTransformation(recyclerView: RecyclerView){
         child.scaleY = scale
     }
 }
+
+//package com.movies.streamy.view.home
+//
+//import android.os.Build
+//import android.os.Bundle
+//import android.util.Log
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.annotation.RequiresApi
+//import androidx.databinding.DataBindingUtil
+//import androidx.fragment.app.Fragment
+//import androidx.lifecycle.Observer
+//import androidx.lifecycle.ViewModelProvider
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import androidx.recyclerview.widget.RecyclerView
+//import com.movies.streamy.R
+//import com.movies.streamy.databinding.FragmentHomeBinding
+//import com.movies.streamy.model.dataSource.network.data.response.homeData.HomeResult
+//import com.movies.streamy.utils.Prefs
+//import dagger.hilt.android.AndroidEntryPoint
+//import kotlinx.coroutines.ExperimentalCoroutinesApi
+//
+//
+//@ExperimentalCoroutinesApi
+//@AndroidEntryPoint
+//class HomeFragment : Fragment() {
+//    private lateinit var binding: FragmentHomeBinding
+//    private lateinit var viewModel: HomeViewModel
+//    private lateinit var prefs: Prefs
+//
+//    private val homeAdapter: HomeAdapter =  HomeAdapter()
+//
+//    @RequiresApi(Build.VERSION_CODES.M)
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+//        prefs = Prefs(requireContext())
+//    }
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+//        binding.viewModel = viewModel
+//        binding.lifecycleOwner = viewLifecycleOwner
+//
+//
+//        viewModel.movieList.observe(viewLifecycleOwner, Observer {movieList ->
+//            Log.d("HomeFragment", "Fetched Movie List: $movieList")
+//            if (movieList.isNullOrEmpty()) {
+//                // Handle empty case if needed
+//            } else {
+////            hideShimmerEffect()
+//                homeAdapter.asyncList.submitList(movieList)
+//                binding.AllShows.apply {
+//                    layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+//                    setHasFixedSize(true)
+//                    adapter = homeAdapter
+//                }
+//            }
+//        })
+//
+//
+//        return binding.root
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//        initViews()
+//    }
+//
+//    private fun initViews() {
+//        showShimmerEffect()
+//        viewModel.getMovieLists()
+//        setUpObservers()
+//        setUpAdapter()
+//    }
+//
+//    private fun setUpObservers() {
+//        viewModel.movieList.observe(viewLifecycleOwner, ::setUpRecyclerView)
+//        viewModel.viewState.observe(viewLifecycleOwner, ::onViewStateChanged)
+//    }
+//    private fun setUpAdapter() {
+//        binding.AllShows.apply {
+//            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+//            setHasFixedSize(true)
+//            adapter = homeAdapter
+//        }
+//    }
+//    private fun setUpRecyclerView(movieList: List<HomeResult?>?) {
+//        Log.d("HomeFragment", "Fetched Movie List: $movieList")
+//        if (movieList.isNullOrEmpty()) {
+//            // Handle empty case if needed
+//        } else {
+////            hideShimmerEffect()
+//            homeAdapter.asyncList.submitList(movieList)
+//            binding.AllShows.apply {
+//                layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+//                setHasFixedSize(true)
+//                adapter = homeAdapter
+//                addOnScrollListener(object : RecyclerView.OnScrollListener() {
+//                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+//                        super.onScrolled(recyclerView, dx, dy)
+//                        applyScaleTransformation(recyclerView)
+//                    }
+//                })
+//            }
+//            binding.AllShows.post{applyScaleTransformation(binding.AllShows)}
+//        }
+//    }
+//    private fun onViewStateChanged(state: HomeViewState) {
+//        hideShimmerEffect()
+//        when (state) {
+//            is HomeViewState.Loading -> {
+//                showShimmerEffect()
+//            }
+//            is HomeViewState.Success -> {
+//                hideShimmerEffect()
+//            }
+//            is HomeViewState.Error -> {
+//                hideShimmerEffect()
+//                // Handle error state
+//            }
+//        }
+//    }
+//
+//    private fun showShimmerEffect() {
+//        binding.shimmerFrameLayout.startShimmer()
+//        binding.shimmerFrameLayout.visibility = View.VISIBLE
+//        binding.AllShows.visibility = View.GONE
+//    }
+//
+//    private fun hideShimmerEffect() {
+//        binding.shimmerFrameLayout.stopShimmer()
+//        binding.shimmerFrameLayout.visibility = View.GONE
+//        binding.AllShows.visibility = View.VISIBLE
+//    }
+//
+//    private fun itemClicked(data: HomeResult) {
+//        // Handle item click
+//    }
+//}
+//
+//private fun applyScaleTransformation(recyclerView: RecyclerView){
+//    val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+//    val midpoint = recyclerView.width/2f
+//    val d0 = 0f
+//    val d1 =0.9f * midpoint
+//    val s0 = 1f
+//    val s1 = 0.7f
+//
+//    for (i in 0 until recyclerView.childCount) {
+//        val child = recyclerView.getChildAt(i)
+//        val childMidpoint = (layoutManager.getDecoratedLeft(child) + layoutManager.getDecoratedRight(child)) / 2f
+//        val d = Math.min(d1, Math.abs(midpoint - childMidpoint))
+//        val scale = s0 + (s1 - s0) * (d - d0) / (d1 - d0)
+//        child.scaleX = scale
+//        child.scaleY = scale
+//    }
+//}
